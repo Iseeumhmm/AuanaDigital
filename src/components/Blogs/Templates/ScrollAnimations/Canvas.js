94437d63bc5bd3c0ead91ee4295b1a2863962fcb
Rick Heffren <rickheffren@Ricks-iMac.local>
2020-02-21 23:44:50 -0500

    plays in line

--- src/components/Blogs/Templates/ScrollAnimations/Canvas.js
+++ src/components/Blogs/Templates/ScrollAnimations/Canvas.js
@@ -19,56 +19,53 @@ const Canvas = (props) => {
   let canvasRef = useRef(null)
   let videoRef = useRef(null)
 
-  useEffect(() => {
-    console.log(videoRef)
-    videoRef.current.load()
-  }, [])
+  // useEffect(() => {
+  //   console.log(videoRef)
+  //   videoRef.current.load()
+  // }, [])
 
-  useEffect(() => {
-    // Draws a square in the middle of the canvas rotated
-    // around the centre by props.angle
-    const { angle } = props;
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    const width = canvas.width;
-    const height = canvas.height;
-    ctx.save();
-    ctx.beginPath();
-    ctx.clearRect(0, 0, width, height);
-    ctx.translate(width / 2, height / 2);
-    ctx.rotate((angle * Math.PI) / 180);
-    ctx.fillStyle = '#4397AC';
-    ctx.fillRect(-width / 4, -height / 4, width / 2, height / 2);
-    ctx.restore();
-  })
+  // useEffect(() => {
+  //   // Draws a square in the middle of the canvas rotated
+  //   // around the centre by props.angle
+  //   const { angle } = props;
+  //   const canvas = canvasRef.current;
+  //   const ctx = canvas.getContext('2d');
+  //   const width = canvas.width;
+  //   const height = canvas.height;
+  //   ctx.save();
+  //   ctx.beginPath();
+  //   ctx.clearRect(0, 0, width, height);
+  //   ctx.translate(width / 2, height / 2);
+  //   ctx.rotate((angle * Math.PI) / 180);
+  //   ctx.fillStyle = '#4397AC';
+  //   ctx.fillRect(-width / 4, -height / 4, width / 2, height / 2);
+  //   ctx.restore();
+  // })
 
   return (
     <PageContainer>
-      <canvas width="300" height="300" ref={canvasRef} />
-      <div id="video_box">
-        <div id="video_overlays"></div>
+      {/* <canvas width="300" height="300" ref={canvasRef} /> */}
         <video ref={videoRef} id="video" width="640" height="264">
           <source src={cinemagraph} type='video/mp4' />
         </video>
-      </div>
     </PageContainer >
 
   )
 }
 
-const Animation = () => {
+// const Animation = () => {
 
-  const [angle, setAngle] = useState(0)
-  const [raf, setRaf] = useState(0)
-  useEffect(() => {
-    setRaf(requestAnimationFrame(() => {
-      setAngle(angle + 1)
-    }))
-    return cancelAnimationFrame(raf)
-  }, [angle])
-  return (
-    <Canvas angle={angle} />
-  )
-}
+//   const [angle, setAngle] = useState(0)
+//   const [raf, setRaf] = useState(0)
+//   useEffect(() => {
+//     setRaf(requestAnimationFrame(() => {
+//       setAngle(angle + 1)
+//     }))
+//     return cancelAnimationFrame(raf)
+//   }, [angle])
+//   return (
+//     <Canvas angle={angle} />
+//   )
+// }
 
-export default Animation
+export default Canvas
